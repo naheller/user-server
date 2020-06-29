4ace0ff40601fab959a77d100346d26c398061af
Nathan Heller <naheller@gmail.com>
2020-06-28 21:00:03 -0400

    Add region to dynamo config

--- dynamo.js
+++ dynamo.js
@@ -3,9 +3,10 @@ const AWS = require('aws-sdk')
 // process.env.AWS_SDK_LOAD_CONFIG = true
 // process.env.AWS_PROFILE = 'UserTableManager'
 
-// AWS.config.update({
-//   credentials: new AWS.SharedIniFileCredentials(),
-// })
+AWS.config.update({
+  region: 'us-east-1',
+  // credentials: new AWS.SharedIniFileCredentials(),
+})
 
 const documentClient = new AWS.DynamoDB.DocumentClient()
 const TABLE_NAME = 'Users'
